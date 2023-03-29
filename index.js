824673be13b366913e28868bb24fe7e563456091
varun8177 <varunergurala9999@gmail.com>
2023-03-29 22:30:40 +0530

    swagger ui removed

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express")
 const Connection = require("./config/db")
 const CartRouter = require("./routes/cart.route")
 const userRouter = require("./routes/user.routes")
-const swaggerJSdoc = require("swagger-jsdoc")
-const swaggerUI = require("swagger-ui-express")
 const cors = require("cors")
 const productRouter = require("./routes/product.route")
 const app = express()
@@ -12,33 +10,15 @@ require("dotenv").config()
 app.use(express.json())
 app.use(cors())
 
+app.get("/", (req, res) => {
+    res.send({
+        message: "Home page"
+    })
+})
 
 app.use("/users", userRouter)
 app.use("/cart", CartRouter)
 app.use("/products", productRouter)
-
-// defination
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Notes app API",
-            version: "1.0.0"
-        },
-        servers: [
-            {
-                url: "https://black-skirt.cyclic.app"
-            }
-        ]
-    },
-    apis: ["./routes/*.js"]
-}
-
-// specification 
-const swaggerSpec = swaggerJSdoc(options)
-
-// building the UI
-app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 
 app.listen(process.env.port, async () => {
